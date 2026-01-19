68afad4af351093aa0c754fc714aa02469503c8d
Vasileios Pipilas <vasileios.pipilas@icloud.com>
2026-01-19 09:59:15 +0200

    #Start of main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+#######################################
+###This is a excel file cross checker##
+#######################################
+
+def user_input_files():
+
+    return
+
+
+def main():
+    # Your core logic will live here
+    print("Program started")
+
+if __name__ == "__main__":
+    main()
